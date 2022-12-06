705d87bbed29350b3ce96597de5f16b3b479b1dd
Karen Jiang <kkarenjjiang.com>
2022-12-06 00:49:51 -0800

    set up

--- dgrealty/src/App.js
+++ dgrealty/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import Router from "./Router";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      hi
+      <Header />
+      <Router />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
